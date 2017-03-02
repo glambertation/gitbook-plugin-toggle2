698ad40ebf86c6d191dba697eac68e528b78dc7d
songhaiyun <songhaiyun@bytedance.com>
2017-03-02 14:00:45 +0800

    增加统计

--- book/toggle.js
+++ book/toggle.js
@@ -21,23 +21,72 @@ require(["gitbook", "jQuery"], function(gitbook, $) {
         chapter.parent().attr('class') != 'book-summary' &&
         chapter.length !=0
       ){
-        
+
         chapter.show();
       }
-    
-      
+
       if (chapter.children().length !=0){
 
         expand2(chapter.children());
 
       }
-          
+
   }
+
+    function toggle2(chapter) {
+
+      if($shy =='1.1'){
+
+            $chapter.click(function(){
+              if(event.target.href=="http://techblog.bytedance.net/"){
+                $chapter.next().children('ul.articles').toggle();
+              }
+            });
+
+      }
+      if($shy =='1.2'){
+
+            $chapter.click(function(){
+                if(event.target.href=="http://techblog.bytedance.net/janfeb17/"){
+                    $chapter.children('ul.articles').toggle();
+                }
+            });
+
+      }
+      if($shy =='1.3'){
+
+            $chapter.click(function(){
+                if(event.target.href=="http://techblog.bytedance.net/internal_techblog/"){
+                    $chapter.children('ul.articles').toggle();
+                }
+            });
+
+      }
+      if($shy =='1.4'){
+
+            $chapter.click(function(){
+                if(event.target.href=="http://techblog.bytedance.net/chapter1/"){
+                    $chapter.children('ul.articles').toggle();
+                }
+            });
+
+      }
+      if($shy =='1.5'){
+
+            $chapter.click(function(){
+                if(event.target.href=="http://techblog.bytedance.net/chapter2/"){
+                    $chapter.children('ul.articles').toggle();
+                }
+            });
+
+      }
+
+    }
 
   gitbook.events.bind("page.change", function() {
 
 // hide all
-    $('li.chapter').children('ul.articles').hide(); 
+    $('li.chapter').children('ul.articles').hide();
 
     $chapter = $('li.chapter.active');
     $shy = $('li.chapter.active').attr("data-level");
@@ -53,17 +102,20 @@ require(["gitbook", "jQuery"], function(gitbook, $) {
     }
 
 // click to toggle
-    $chapter.click(function(){
-      $chapter.children('ul.articles').toggle();
-    });
 
-    if($shy =='1.1'){
-      $chapter.click(function(){
-        $chapter.next().children('ul.articles').toggle();
-      });
+    toggle2($chapter);
+    $hai = $('li.chapter.active').attr("data-level");
 
-    }
-
+//add
+    !function (e, t, n, a, c) {
+      e.ToutiaoAnalyticsObject = n, e[n] = e[n] || function () {
+        (e[n].q = e[n].q || []).push(arguments)
+      }, e[n].t = 1 * new Date, e[n].s = c;
+      var s = t.createElement("script");
+      s.async = 1, s.src = a, t.getElementsByTagName("head")[0].appendChild(s)
+    }(window, document, "ba", "//s3.bytecdn.cn/ta/resource/v0/analytics.js?v=0.2.0");
+    ba("create", "70f1bc699510e4");
+    ba("send", "pageview");
 
   });
 
